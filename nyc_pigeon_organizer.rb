fbda962a97d1bde37d9ef33c702f56efb350c8a5
Joshua Ticzon <ticzon.joshua@gmail.com>
2017-11-29 23:24:35 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -14,6 +14,7 @@ def nyc_pigeon_organizer(data)
             hash[name] = {data.keys[i] => [specificStat[0]]}
           else
             hash[name] = {data.keys[i] => [specificStat[0]] << [specificStat[0]]}
+            end
           end
         end
       end
