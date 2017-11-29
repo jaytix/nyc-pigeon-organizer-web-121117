161deac8aa52566bb8d6868cf4794de58ebaef5f
Joshua Ticzon <ticzon.joshua@gmail.com>
2017-11-29 21:22:53 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,9 +5,9 @@ def nyc_pigeon_organizer(data)
   data.each_with_index do |stats, i|
     #binding.pry
     stats[1].each do |specificStat, oneStat|
-      #binding.pry
+      binding.pry
       specificStat[1].each do |name|
-       binding.pry
+       #binding.pry
          hash[name] = {data.keys[i] => oneStat}
       end
     end
