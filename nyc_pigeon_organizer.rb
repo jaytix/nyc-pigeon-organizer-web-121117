898c35b2f43dc2c65079109da0b70eda9eed20a0
Joshua Ticzon <ticzon.joshua@gmail.com>
2017-11-30 00:39:38 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,23 +1,30 @@
 require 'pry'
 
+# def nyc_pigeon_organizer(data)
+#   hash = Hash.new
+#   #array = []
+#   data.each_with_index do |stats, i|
+#     #binding.pry
+#     stats[1].each_with_index do |specificStat, j|
+#       #binding.pry
+#       specificStat[1].each do |name|
+#        #binding.pry
+#           if !hash.keys.include?(name)
+#             #array.push(specificStat[0])
+#             hash[name] = {data.keys[i] => [stats[1].keys[j]]}
+#           elsif hash.keys.include?(name)
+#             binding.pry
+#             hash[name].values << [stats[1].keys[j]]
+#         end
+#       end
+#     end
+#   end
+#   puts hash
+# end
+
 def nyc_pigeon_organizer(data)
-  hash = Hash.new
-  #array = []
-  data.each_with_index do |stats, i|
-    #binding.pry
-    stats[1].each_with_index do |specificStat, j|
-      #binding.pry
-      specificStat[1].each do |name|
-       #binding.pry
-          if !hash.keys.include?(name)
-            #array.push(specificStat[0])
-            hash[name] = {data.keys[i] => [stats[1].keys[j]]}
-          elsif hash.keys.include?(name)
-            binding.pry
-            hash[name].values << [stats[1].keys[j]]
-        end
-      end
-    end
+  data.each_with_obj(Hash.new { |a, b| a[b] = data })
+    binding.pry
   end
-  puts hash
+
 end
